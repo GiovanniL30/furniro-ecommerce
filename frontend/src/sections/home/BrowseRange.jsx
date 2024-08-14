63398b5ec143f3d26b5c32b872a5d34b9d9f303d
Giovanni Leo <2232254@slu.edu.ph>
2024-08-14 21:00:21 +0800

    browse range section

--- frontend/src/sections/home/BrowseRange.jsx
+++ frontend/src/sections/home/BrowseRange.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import dining from '../../assets/dining-img.png'
+import living from '../../assets/living-img.png'
+import bedroom from '../../assets/bedroom-img.png'
+
+const items = [
+  {
+    name: 'Dining',
+    img: dining,
+  },
+  {
+    name: 'Living',
+    img: living,
+  },
+  {
+    name: 'Bedroom',
+    img: bedroom,
+  },
+]
 
 const BrowseRange = () => {
-  return <div className='max-container'>BrowseRange</div>
+  return (
+    <div className='max-container flex flex-col justify-center items-center'>
+      <div className='mb-12'>
+        <h1 className='font-bold text-center tracking-wide pb-1 text-2xl text-font_color'>
+          Browse The Range
+        </h1>
+        <p className='text-grey_2 text-center'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+          modi?
+        </p>
+      </div>
+
+      <div className='w-full flex gap-10 flex-wrap lg:flex-nowrap justify-center lg:justify-between'>
+        {items.map((item) => (
+          <ItemCard name={item.name} img={item.img} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const ItemCard = ({ name, img }) => {
+  return (
+    <div className='flex flex-col gap-2 items-center justify-center w-full'>
+      <div className='lg:w-full'>
+        <img className='rounded-lg object-cover w-full' src={img} alt={name} />
+      </div>
+      <p className='font-semibold text-font-color'>{name}</p>
+    </div>
+  )
 }
 
 export default BrowseRange
